0717e2afb6057d06a6d1ee7b7ffe0c6317d7913e
Jake <jws337@cs.drexel.edu>
2015-08-02 18:42:14 -0400

    Initial attempt at logging, use it plebs

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
-	"os"
+	//	"os"
+	"fmt"
+	"github.com/spf13/viper"
 
-	"foodtastechess/server"
+	//	"foodtastechess/server"
+	"foodtastechess/logger"
 )
 
-func main() {
-	s := server.New()
+func loggingConf() {
+	var C logger.LoggerConfig
+	err := viper.MarshalKey("logger", &C)
+	if err != nil {
+		panic(fmt.Errorf("Can't parse: %s \n", err))
+	}
+	logger.InitLog(C)
+	log := logger.Log("main")
+	log.Debug("I did it")
+	log.Critical("Holy shit")
+}
 
-	s.Serve("0.0.0.0", os.Getenv("PORT"))
+func readConf() {
+	viper.SetConfigName("config")
+	//	viper.AddConfigPath("/etc/appname/") you can use multiple search paths
+	viper.AddConfigPath("./")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	loggingConf()
+}
+
+func main() {
+	//	s := server.New()
+
+	//	s.Serve("0.0.0.0", os.Getenv("PORT"))
+	readConf()
 }
