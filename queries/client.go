0d1cd7c1e26fb69a8fa72f222d4f9efb1bf72980
g. nicholas d'andrea <gnidan@dramafever.com>
2015-08-03 00:43:24 -0400

    Adding some comments

--- queries/client.go
+++ queries/client.go
@@ -4,20 +4,30 @@ import (
 	"foodtastechess/game"
 )
 
+// ClientQueries is the interface by which other parts of the system
+// may get information about current and past states of games.
 type ClientQueries interface {
 	GameInformation(id game.Id) GameInformation
 }
 
+// ClientQueryService provides a concrete implementation of the
+// ClientQueries interface.
+//
+// It composes an injected SystemQueries interface that it uses to
+// aggregate information for the supported methods
 type ClientQueryService struct {
 	SystemQueries SystemQueries `inject:"systemQueries"`
 }
 
+// GameInformation is a structural representation of the current
+// state of a game.
 type GameInformation struct {
 	Id         game.Id
 	TurnNumber game.TurnNumber
 	BoardState game.FEN
 }
 
+// GameInformation accepts a game ID and queries the SQS for GameInformation
 func (s *ClientQueryService) GameInformation(id game.Id) GameInformation {
 	var (
 		turnNumberQuery Query           = TurnNumberQuery(id)
