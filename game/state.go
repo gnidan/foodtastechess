405b93ce97069ca110805e1a0734f216f0008243
zachpepsin <zachpepsin@gmail.com>
2015-08-04 14:28:32 -0400

    Update state.go

--- game/state.go
+++ game/state.go
@@ -1,18 +1,19 @@
 package game
 
 type GameState struct{
-	pieceMap [Position]Piece
+	pieceMap map[Position]Piece
 }
 
 type Position struct{
 	file, rank int
 }
 
-func (s *GameState) PieceAtPosition(pos Position) piece Piece{
-	piece, ok := pieceMap.Get(pos)
-	if(ok == nul){
-		//TODO error handling
-	}else {
+func (s *GameState) PieceAtPosition(pos Position) Piece{
+	piece, ok := s.pieceMap[pos]
+	if( ok ){
+		//piece found
 		return piece
 	}
+	//piece not found
+	return nil
 }
