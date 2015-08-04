55d576876122c2e12980936663f4e7018fd722ef
zachpepsin <zachpepsin@gmail.com>
2015-08-04 14:28:14 -0400

    Update piece.go

--- game/piece.go
+++ game/piece.go
@@ -16,60 +16,60 @@ type Pawn struct {
 	color Color
 	moves []Move
 }
-func (p *Pawn) Color{ return p.color }
-func (p *Pawn) Moves(){ return p.moves }
+func (p *Pawn) Color() Color{ return p.color }
+func (p *Pawn) Moves() []Move{ return p.moves }
 
 type Rook struct {
 	color Color
 	moves []Move
 }
-func (r *Rook) Color{ return r.color }
-func (r *Rook) Moves(){ return r.moves }
+func (r *Rook) Color() Color{ return r.color }
+func (r *Rook) Moves() []Move{ return r.moves }
 
 type Knight struct {
 	color Color
 	moves []Move
 }
-func (n *Knight) Color{ return n.color }
-func (n *Knight) Moves(){ return n.moves }
+func (n *Knight) Color() Color{ return n.color }
+func (n *Knight) Moves() []Move{ return n.moves }
 
 type Bishop struct {
 	color Color
 	moves []Move
 }
-func (b *Bishop) Color{ return b.color }
-func (b *Bishop) Moves(){ return b.moves }
+func (b *Bishop) Color() Color{ return b.color }
+func (b *Bishop) Moves() []Move{ return b.moves }
 
 type Queen struct {
 	color Color
 	moves []Move
 }
-func (q *Queen) Color{ return q.color }
-func (q *Queen) Moves(){ return q.moves }
+func (q *Queen) Color() Color{ return q.color }
+func (q *Queen) Moves() []Move{ return q.moves }
 
 type King struct {
 	color Color
 	moves []Move
 }
-func (k *King) Color{ return k.color }
-func (k *King) Moves(){ return k.moves }
+func (k *King) Color() Color{ return k.color }
+func (k *King) Moves() []Move{ return k.moves }
 
 
 
 func NewPawn(color Color) Pawn{
 	p := Pawn{}
 	p.color = color
-	p.moves := [...]{
-		new AdvancingMove(1, /*TODO: based on color */), //move forward
-		new CapturingMove(-1, 1), new CapturingMove(1, 1), //Diagonal capturing
+	p.moves = []Move{
+		&AdvancingMove{ 1 /*TODO: based on color */ }, //move forward  
+		&CapturingMove{ 1, 1}, &CapturingMove{1, 1}, //Diagonal capturing
 		}
 	if(p.color == White){
-		p.moves.append( 
-			new EnPassantMove(-1, 1), new EnPassantMove(1, 1)
+		p.moves = append( p.moves,
+			&EnPassantMove{-1, 1}, &EnPassantMove{1, 1},
 		)
 	}else{
-		p.moves.append( 
-			new EnPassantMove(-1, -1), new EnPassantMove(1, -1)
+		p.moves = append( p.moves, 
+			&EnPassantMove{-1, -1}, &EnPassantMove{1, -1},
 		)
 	}
 	return p
@@ -78,10 +78,10 @@ func NewPawn(color Color) Pawn{
 func NewRook(color Color) Rook{
 	r := Rook{}
 	r.color = color
-	r.moves := [...]{
-		new UnboundMove(1, 0), new UnboundMove(0, -1), //Horizontals
-		new UnboundMove(0, 1), new UnboundMove(0, -1), //Verticals
-		new CastlingMove( /*TODO*/)
+	r.moves = []Move{
+		&UnboundMove{1, 0}, &UnboundMove{0, -1}, //Horizontals
+		&UnboundMove{0, 1}, &UnboundMove{0, -1}, //Verticals
+		&CastlingMove{ /*TODO*/},
 		}
 	return r
 }//NewRook
@@ -89,11 +89,11 @@ func NewRook(color Color) Rook{
 func NewKnight(color Color) Knight{
 	n := Knight{}
 	n.color = color
-	n.moves := [...]{
-		new BoundMove(-2, 1), new BoundMove(-1, 2), //up-left
-		new BoundMove(1, 2), new BoundMove(2, 1), //up-right
-		new BoundMove(-2, 1), new BoundMove(-1, 2), //down-left
-		new BoundMove(-2, 1), new BoundMove(-1, 2) //down-right
+	n.moves = []Move{
+		&BoundMove{-2, 1}, &BoundMove{-1, 2}, //up-left
+		&BoundMove{1, 2}, &BoundMove{2, 1}, //up-right
+		&BoundMove{-2, 1}, &BoundMove{-1, 2}, //down-left
+		&BoundMove{-2, 1}, &BoundMove{-1, 2}, //down-right
 		}
 	return n
 }//NewKnight
@@ -101,9 +101,9 @@ func NewKnight(color Color) Knight{
 func NewBishop(color Color) Bishop{
 	b := Bishop{}
 	b.color = color
-	b.moves := [...]{
-		new UnboundMove(-1, 1), new UnboundMove(1, 1), //up diagonals
-		new UnboundMove(-1, -1), new UnboundMove(1, -1) //down diagonals
+	b.moves = []Move{
+		&UnboundMove{-1, 1}, &UnboundMove{1, 1}, //up diagonals
+		&UnboundMove{-1, -1}, &UnboundMove{1, -1}, //down diagonals
 		}
 	return b
 }//NewBishop
@@ -111,10 +111,10 @@ func NewBishop(color Color) Bishop{
 func NewQueen(color Color) Queen{
 	q := Queen{}
 	q.color = color
-	q.moves := [...]{
-		new UnboundMove(-1, 1), new UnboundMove(0, 1), new UnboundMove(1, 1), //forward
-		new UnboundMove(-1, 0), new UnboundMove(1, 0), //sideways
-		new UnboundMove(-1, -1), new UnboundMove(0, -1), new UnboundMove(1, -1) //backwards
+	q.moves = []Move{
+		&UnboundMove{-1, 1}, &UnboundMove{0, 1}, &UnboundMove{1, 1}, //forward
+		&UnboundMove{-1, 0}, &UnboundMove{1, 0}, //sideways
+		&UnboundMove{-1, -1}, &UnboundMove{0, -1}, &UnboundMove{1, -1}, //backwards
 		}
 	return q
 }//NewQueen
@@ -122,10 +122,10 @@ func NewQueen(color Color) Queen{
 func NewKing(color Color) King{
 	k := King{}
 	k.color = color
-	k.moves := [...]{
-		new BoundMove(-1, 1), new BoundMove(0, 1), new BoundMove(1, 1), //forward
-		new BoundMove(-1, 0), new BoundMove(1, 0), //sideways
-		new BoundMove(-1, -1), new BoundMove(0, -1), new BoundMove(1, -1) //backwards
+	k.moves = []Move{
+		&BoundMove{-1, 1}, &BoundMove{0, 1}, &BoundMove{1, 1}, //forward
+		&BoundMove{-1, 0}, &BoundMove{1, 0}, //sideways
+		&BoundMove{-1, -1}, &BoundMove{0, -1}, &BoundMove{1, -1}, //backwards
 		}
 	return k
 }//NewKing
