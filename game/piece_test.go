a266ea01d5cd22acadea35021caa62228535bbb7
zachpepsin <zachpepsin@gmail.com>
2015-08-06 16:38:37 -0400

    Create piece_test.go

---
+++ game/piece_test.go
@@ -0,0 +1,25 @@
+package game
+
+import (
+	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type PieceTestSuite struct {
+	suite.Suite
+}
+
+func TestPieceTestSuite(t *testing.T) {
+	suite.Run(t, new(PieceTestSuite))
+}
+
+func (s *PieceTestSuite) TestPieceConstructor() {
+	pawn := NewPawn(Black)
+
+	assert := assert.New(s.T())
+	//assert.Equal(expected, actual)
+	//assert.Equal(1,0) //force fail
+	assert.Equal(pawn.color, Black)
+}
