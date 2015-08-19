bcaa6c35fc7e6ba5f68cbf430f431d1b80636066
Jake <jws337@cs.drexel.edu>
2015-08-18 22:11:43 -0400

    Fleshed out api's

--- server/api/api.go
+++ server/api/api.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
 	"net/http"
+	"strconv"
 
+	"foodtastechess/game"
 	"foodtastechess/logger"
 	"foodtastechess/queries"
 	"foodtastechess/users"
@@ -58,24 +60,55 @@ func (api *ChessApi) Handler() http.Handler {
 
 func (api *ChessApi) GetGames(res rest.ResponseWriter, req *rest.Request) {
 	u := getUser(req)
-	res.WriteJson(fmt.Sprintf("Hello, %s!", u.Name))
+	res.WriteJson(api.ClientQueries.UserGames(u))
 }
 
 func (api *ChessApi) GetGameInfo(res rest.ResponseWriter, req *rest.Request) {
-	u := getUser(req)
 	id := req.PathParam("id")
+	intId, err := strconv.Atoi(id)
+	gameId := game.Id(intId)
+	if err != nil
+	{
+		log.debug("Recieved an invalid gameid, it was not an int: %s", val)
+		rest.Error(res,err.Error(),http.StatusNotFound)
+	}
+	else
+	{
+		res.WriteJson(api.ClientQueries.GameInformation(intId))
+	}
 }
 
 func (api *ChessApi) GetGameHistory(res rest.ResponseWriter, req *rest.Request) {
-	u := getUser(req)
 	id := req.PathParam("id")
+	intId, err := strconv.Atoi(id)
+	gameId := game.Id(intId)
+	if err != nil
+	{
+		log.debug("Recieved an invalid gameid, it was not an int: %s", val)
+		rest.Error(res,err.Error(),http.StatusNotFound)
+	}
+	else
+	{
+		res.WriteJson(api.ClientQueries.GameHistory(intId))
+	}
 }
 
 func (api *ChessApi) GetGameValidMoves(res rest.ResponseWriter, req *rest.Request) {
-	u := getUser(req)
 	id := req.PathParam("id")
+	intId, err := strconv.Atoi(id)
+	gameId := game.Id(intId)
+	if err != nil
+	{
+		log.debug("Recieved an invalid gameid, it was not an int: %s", val)
+		rest.Error(res,err.Error(),http.StatusNotFound)
+	}
+	else
+	{
+		res.WriteJson(api.ClientQueries.ValidMoves(intId))
+	}
 }
 
 func getUser(req *rest.Request) users.User {
 	return req.Env["user"].(users.User)
 }
+
