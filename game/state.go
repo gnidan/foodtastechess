c1d27d2e8c12448d3efa3a1cafbce25641bf0106
zachpepsin <zachpepsin@gmail.com>
2015-08-19 16:54:40 -0400

    starting on ConvertToState()

--- game/state.go
+++ game/state.go
@@ -36,16 +36,15 @@ func (s *GameState) PieceAtPosition(pos Position) Piece {
 	return nil
 }
 
-func (s GameState) ConvertToFEN() string {
+func (s GameState) ConvertToFEN() FEN {
 	stringFEN := ""
-	
+
 	for rank := 8; rank > 0; rank-- {
 		emptyCount := 0
 		for file := 1; file <= 8; file++ {
 
 			piece := s.PieceAtPosition(Position{file, rank})
 
-			//fmt.Println(reflect.TypeOf(piece))
 			if piece == nil {
 				emptyCount++
 				continue
@@ -100,7 +99,90 @@ func (s GameState) ConvertToFEN() string {
 		}
 	}
 
-	return stringFEN
+	newFEN := FEN(stringFEN)
+	return newFEN
+}
+
+func (fen FEN) ConvertToState() GameState {
+	stringFEN := string(fen)
+	gameState := GameState{}
+	pieceMap := map[Position]Piece{}
+
+	file := 1
+	rank := 8
+
+	// 1) PIECE PLACEMENT - from white prespective, starts at rank 8 (top)
+	//		Within each rank, each piece is described with it's letter (uppercase for White)
+	//		Number of consecutive open spaces in a rank are denoted by a digit (1 through 8)
+	//		Each rank is separated by a '/'
+	for rank >= 1 {
+		next := stringFEN[:1]
+		nextInt, _ := strconv.Atoi(next)
+		stringFEN = stringFEN[1:len(stringFEN)]
+
+		//Pieces
+		if next == "P" {
+			pieceMap[NewPosition(file, rank)] = NewPawn(White)
+			file += 1
+		} else if next == "p" {
+			pieceMap[NewPosition(file, rank)] = NewPawn(Black)
+			file += 1
+		} else if next == "N" {
+			pieceMap[NewPosition(file, rank)] = NewKnight(White)
+			file += 1
+		} else if next == "n" {
+			pieceMap[NewPosition(file, rank)] = NewKnight(Black)
+			file += 1
+		} else if next == "B" {
+			pieceMap[NewPosition(file, rank)] = NewBishop(White)
+			file += 1
+		} else if next == "b" {
+			pieceMap[NewPosition(file, rank)] = NewBishop(Black)
+			file += 1
+		} else if next == "R" {
+			pieceMap[NewPosition(file, rank)] = NewRook(White)
+			file += 1
+		} else if next == "r" {
+			pieceMap[NewPosition(file, rank)] = NewRook(Black)
+			file += 1
+		} else if next == "Q" {
+			pieceMap[NewPosition(file, rank)] = NewQueen(White)
+			file += 1
+		} else if next == "q" {
+			pieceMap[NewPosition(file, rank)] = NewQueen(Black)
+			file += 1
+		} else if next == "K" {
+			pieceMap[NewPosition(file, rank)] = NewKing(White)
+			file += 1
+		} else if next == "k" {
+			pieceMap[NewPosition(file, rank)] = NewKing(Black)
+			file += 1
+		} else if nextInt > 0 && nextInt <= 8 {
+			//Digit 1-8, blank spaces
+			file += nextInt
+		}
+
+		if file > 8 {
+			//end of file, go to next rank down
+			rank -= 1
+			file = 1
+		}
+
+	}
+
+	// 2) ACTIVE COLOR -  'w' indicated white moves next, 'b' indicates black
+
+	// 3) CASTLING AVAILABILITY - K/Q/k/q for Black/White King/Queenside; '-' for neither
+
+	// 4) EN PASSANT TARGET SQUARE - in algabraic notation.  If none, then it is '-'.
+	//			Recorded as space behind pawn after a double move (even if an opponent isn't near)
+
+	// 5) HALFMOVE CLOCK - number of halfmoves since last capture or pawn advance. Used for 50-move rule
+
+	// 6) FULLMOVE NUMBER - starts at 1, increments after Black's move
+
+	gameState = NewGameState(pieceMap)
+	return gameState
 }
 
 func InitializeBoard() GameState {
