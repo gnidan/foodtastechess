37df3e78c60162f7c5aad7cb94c7dfa0cc12b303
zachpepsin <zachpepsin@gmail.com>
2015-08-20 15:53:10 -0400

    Update state_test.go

--- game/state_test.go
+++ game/state_test.go
@@ -5,7 +5,6 @@ import (
 	//"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"testing"
-	//"strconv"
 )
 
 type GameStateTestSuite struct {
@@ -17,35 +16,11 @@ func TestGameStateTestSuite(t *testing.T) {
 }
 
 func (s *GameStateTestSuite) TestMapConstructor() {
-	/*
-		pieceMap := map[Position]Piece{}
-		pieceMap[NewPosition(2, 2)] = NewPawn(Black)
 
-		state := NewGameState(pieceMap)
-		assert := assert.New(s.T())
-		//assert.Equal(expected, actual)
-		//assert.Equal(1,0) //force fail
-		assert.Equal(state.PieceAtPosition(Position{2, 2}), NewPawn(Black))
-	*/
-
-	state := InitializeBoard()
-	//pieceMap := map[Position]Piece{}
-	//pieceMap[NewPosition(2, 2)] = NewPawn(Black)
-	//state := NewGameState(pieceMap)
-	newFEN := state.ConvertToFEN()
-
-	newStateFromFEN := newFEN.ConvertToState()
-
+	newFEN := InitializeFEN()
+	stateFromFEN := newFEN.ConvertToState()
+	stateFromOG := InitializeState()
 	assert := assert.New(s.T())
-	assert.Equal(state, newStateFromFEN)
-	//assert.Equal(state, newFEN)
+	assert.Equal(stateFromOG, stateFromFEN)
 
-	/*
-		stringFEN := "GBCDEFG"
-		next := stringFEN[:1]
-
-		temp,_ := strconv.Atoi(next)
-		assert := assert.New(s.T())
-		assert.Equal("...", temp)
-	*/
 }
