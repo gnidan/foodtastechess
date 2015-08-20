37fb981a509cf2dcf434dcb118876f092dbc19b8
zachpepsin <zachpepsin@gmail.com>
2015-08-20 15:51:52 -0400

    added ExecuteMove

--- game/move.go
+++ game/move.go
@@ -1,5 +1,9 @@
 package game
 
+import (
+	"strconv"
+)
+
 //BoundMove - moves max of one position offset
 //UnboundMove - moves until hit own piece, border, or opponent + 1
 //AdvancingMove - Unique to Pawns: like bound move, but no capture allowed
@@ -38,12 +42,146 @@ type EnPassantMove struct {
 type CastlingMove struct {
 }
 
-//Takes FEN and Algebraic, executes move and returns string
-func ExecuteMove(stringFEN string, stringAlgabraic string) string {
-	newStringFEN := ""
+//Takes AlgebraicMove and FEN, executes move and returns resulting FEN
+func ExecuteMove(algebraicMove AlgebraicMove, prevFEN FEN) FEN {
+	state := prevFEN.ConvertToState()
+	state.enPassantTarget = ""
+	state.halfmoveClock += 1
+	color := state.activeColor
+	newPieceMap := state.pieceMap
+	stringAN := string(algebraicMove)
 
-	return newStringFEN
+	if stringAN == "0-0" {
+		// KINGSIDE CASTLING:
+		castleRank := 0
+		if color == White {
+			castleRank = 1
+		} else {
+			castleRank = 8
+		}
+		newPieceMap[NewPosition(5, castleRank)] = nil
+		newPieceMap[NewPosition(8, castleRank)] = nil
+		newPieceMap[NewPosition(7, castleRank)] = NewKing(color)
+		newPieceMap[NewPosition(6, castleRank)] = NewRook(color)
+
+	} else if stringAN == "0-0-0" {
+		// QUEENSIDE CASTLING:
+		castleRank := 0
+		if color == White {
+			castleRank = 1
+		} else {
+			castleRank = 8
+		}
+		newPieceMap[NewPosition(5, castleRank)] = nil
+		newPieceMap[NewPosition(1, castleRank)] = nil
+		newPieceMap[NewPosition(3, castleRank)] = NewKing(color)
+		newPieceMap[NewPosition(4, castleRank)] = NewRook(color)
+
+	} else {
+		pieceType := stringAN[:1]            // P/N/B/R/Q/K
+		stringAN = stringAN[1:len(stringAN)] //Remove origPiece char
+		origFile := stringAN[:1]
+		stringAN = stringAN[1:len(stringAN)] //Remove origFile char
+		origRank := stringAN[:1]
+		stringAN = stringAN[1:len(stringAN)] //Remove origRank char
+		moveType := stringAN[:1]             // - (no capture)/x (capture)/ = (pawn promotion)
+		stringAN = stringAN[1:len(stringAN)] //Remove moveType char
+
+		origFileInt := fileToInt(origFile)
+		origRankInt, _ := strconv.Atoi(origRank)
+		origPosition := NewPosition(origFileInt, origRankInt)
+
+		//piece is moving
+		nextFile := stringAN[:1]
+		stringAN = stringAN[1:len(stringAN)] //Remove nextFile char
+		nextRank := stringAN[:1]
+		stringAN = stringAN[1:len(stringAN)] //Remove nextRank char
+
+		nextFileInt := fileToInt(nextFile)
+		nextRankInt, _ := strconv.Atoi(nextRank)
+		nextPosition := NewPosition(nextFileInt, nextRankInt)
+
+		//halfmoveClock resets on capture
+		if moveType == "x" {
+			state.halfmoveClock = 0
+		}
+
+		newPieceMap[origPosition] = nil
+
+		if pieceType == "P" {
+			newPieceMap[nextPosition] = NewPawn(color)
+			state.halfmoveClock = 0 //halfmoveClock resets on pawn advance
+			rankDelta := nextRankInt - origRankInt
+			if rankDelta == 2 || rankDelta == -2 { //pawn moved out 2, en passant target
+				targetPos := origFile
+				if origRankInt == 2 {
+					targetPos += "3"
+				} else {
+					targetPos += "6"
+				}
+				state.enPassantTarget = AlgebraicMove(targetPos)
+			}
+			if len(stringAN) > 0 {
+				//PAWN PROMOTION
+				stringAN = stringAN[1:len(stringAN)] //Remove '='
+				promoteType := stringAN[:1]          // Q/N/R/B
+
+				if promoteType == "Q" {
+					newPieceMap[nextPosition] = NewQueen(color)
+				} else if promoteType == "N" {
+					newPieceMap[nextPosition] = NewKnight(color)
+				} else if promoteType == "R" {
+					newPieceMap[nextPosition] = NewRook(color)
+				} else if promoteType == "B" {
+					newPieceMap[nextPosition] = NewBishop(color)
+				}
+			}
+		} else if pieceType == "N" {
+			newPieceMap[nextPosition] = NewKnight(color)
+		} else if pieceType == "B" {
+			newPieceMap[nextPosition] = NewBishop(color)
+		} else if pieceType == "R" {
+			newPieceMap[nextPosition] = NewRook(color)
+		} else if pieceType == "Q" {
+			newPieceMap[nextPosition] = NewQueen(color)
+		} else if pieceType == "K" {
+			newPieceMap[nextPosition] = NewKing(color)
+		}
+
+	} //esle
+
+	//activeColor change
+	if color == White {
+		state.activeColor = Black
+	} else {
+		state.activeColor = White
+		state.fullmoveNumber += 1 //fullmove number increments after black turn
+	}
+
+	newFEN := state.ConvertToFEN() //convert back to FEN for return
+	return newFEN
 }
+
+func fileToInt(file string) int {
+	if file == "a" || file == "A" {
+		return 1
+	} else if file == "b" || file == "B" {
+		return 2
+	} else if file == "c" || file == "C" {
+		return 3
+	} else if file == "d" || file == "D" {
+		return 4
+	} else if file == "e" || file == "E" {
+		return 5
+	} else if file == "f" || file == "F" {
+		return 6
+	} else if file == "g" || file == "G" {
+		return 7
+	} else if file == "h" || file == "H" {
+		return 8
+	}
+	return 0
+} //fileToInt
 
 func (s *GameState) ValidMoves(pos Position) []Position {
 	validMoves := []Position{} //make empty array of positions
@@ -74,29 +212,6 @@ func (m *BoundMove) Translate(pos Position, s *GameState) []Position {
 	}
 	return nil
 }
-
-/*
-func (m *UnboundMove) Translate(pos Position, s *GameState) []Position {
-	newPos := Position{
-		file: pos.file + m.fileOffset,
-		rank: pos.rank + m.rankOffset,
-	}
-	//make sure position is within boundaries
-	if newPos.file < 1 || newPos.file > 8 || newPos.rank < 1 || newPos.rank > 8 {
-		return nil
-	}
-	//if no piece at position, it is valid
-	if s.PieceAtPosition(newPos) == nil {
-		return append([]Position{newPos}, m.Translate(newPos, s)...)
-	}
-	//there is a piece in next position
-	if s.PieceAtPosition(newPos).Color() != s.PieceAtPosition(pos).Color() {
-		//if opponent is in position, it is valid, but end of recursion
-		return []Position{newPos}
-	}
-	return nil
-}
-*/
 
 func (m *UnboundMove) Translate(pos Position, s *GameState) []Position {
 
